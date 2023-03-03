5b0f27dfa24fd03695febfe4c423d9dbe1b01048
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-03 12:35:27 +0100

    Insert playlist

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -1,10 +1,25 @@
 import './CreatePlaylist.css';
+import useKeypress from '../hooks/useKeypress';
 
 function CreatePlaylist() {
+
+    function onClick(e) {
+        alert("Create Playlist");
+    }
+
+    useKeypress((key) => {
+        if (key === "Enter")
+            onClick();
+    });
 
     return (
         <div className="create-playlist">
             <h1>Create a Playlist</h1>
+            <h2>Put the name</h2>
+            <div class="create-playlist-input">
+                <input type="text" />
+                <i class="icon-plus" onClick={onClick}></i>
+            </div>
         </div >
     );
 }
