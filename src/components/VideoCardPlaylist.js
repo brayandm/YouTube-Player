ff8ff8a0c3603e83d9691d33ad7e0e129adf4dd7
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-05 22:51:25 +0100

    update in styles

--- src/components/VideoCardPlaylist.js
+++ src/components/VideoCardPlaylist.js
@@ -3,7 +3,11 @@ import './VideoCardPlaylist.css';
 function VideoCardPlaylist({ video }) {
     return (
         <div className="video-card-playlist">
-            <img className="video-thumbnail-playlist" src={video.thumbnail} alt={video.title} />
+            <div className="video-card-playlist-container-img">
+                <img className="video-thumbnail-playlist" src={video.thumbnail} alt={video.title} />
+                <p className='video-card-playlist-views'> {video.views} </p>
+                <p className='video-card-playlist-title'> {video.title.length > 50 ? video.title.substring(0, 50) + "..." : video.title} </p>
+            </div>
         </div>
     );
 }
