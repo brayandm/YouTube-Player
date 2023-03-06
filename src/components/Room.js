590754e63185cf40879d1a2d35e88fe0d76c8ba7
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-06 12:20:21 +0100

    room added

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,6 +2,8 @@ import './Room.css';
 import useSWR from 'swr';
 import YouTube from 'react-youtube';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useRef, useState } from 'react';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -9,10 +11,15 @@ function Room() {
 
     const { roomId } = useParams();
 
+    const [timeKey, setTimeKey] = useState(0);
+
     const { data, error, isLoading } = useSWR(
         `https://youtube.thorsteinsson.is/api/playlists/${roomId}`,
-        fetcher
+        fetcher,
+        { refreshInterval: 500 }
     );
+
+    const playerRef = useRef(null);
 
     const opts = {
         height: '500',
@@ -22,11 +29,40 @@ function Room() {
         },
     };
 
+    function onStateChange(e) {
+
+        if (e.data !== 2) {
+            return;
+        }
+
+        axios.put(`https://youtube.thorsteinsson.is/api/playlists/${roomId}`, {
+            "name": "Room",
+            "videos": [
+                {
+                    videoId: data.videos[0].videoId,
+                    currentTime: e.target.getCurrentTime(),
+                    timeKey: data.videos[0].timeKey + 1
+                }]
+        })
+    }
+
+    if (data && playerRef.current) {
+        if (data.videos[0].timeKey > timeKey) {
+            playerRef.current.internalPlayer.seekTo(data.videos[0].currentTime);
+            setTimeKey(data.videos[0].timeKey);
+        }
+    }
+
     return (
         <div className="room">
             {error && <div></div>}
             {isLoading && <div></div>}
-            {data && <YouTube videoId={data.videoId} opts={opts} />}
+            {data && <YouTube
+                ref={playerRef}
+                videoId={data.videos[0].videoId}
+                opts={opts}
+                onStateChange={onStateChange}
+            />}
         </div >
     );
 }
