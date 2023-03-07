f7859c06ab168502477bcf0ce982bb9f7e920568
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-07 09:38:55 +0100

    backgroundBlur added

--- src/components/OptionsMenu.js
+++ src/components/OptionsMenu.js
@@ -1,31 +1,35 @@
 import './OptionsMenu.css';
 import { Link } from 'react-router-dom';
+import BackgroundBlur from './BackgroundBlur';
 
 function OptionsMenu({ hidden, onClick }) {
 
     return (
-        <div className={"options-menu " + (hidden ? "hidden" : "")}>
-            <div className="options-menu-list">
-                <div className="options-menu-section">
+        <>
+            <BackgroundBlur hidden={hidden} onclick={onClick} />
+            <div className={"options-menu " + (hidden ? "hidden" : "")}>
+                <div className="options-menu-list">
+                    <div className="options-menu-section">
+                    </div>
+
+                    <Link to="/playlists" className='options-menu-item-link' onClick={onClick}>
+                        <div className="options-menu-item">
+                            <i className="icon-music"></i>
+                            <span className="options-menu-text">Go to Playlists</span>
+                            <i className="icon-chevron-right"></i>
+                        </div>
+                    </Link>
+
+                    <Link to="/playlists/create" className='options-menu-item-link' onClick={onClick}>
+                        <div className="options-menu-item">
+                            <i className="icon-plus"></i>
+                            <span className="options-menu-text">Create a Playlist</span>
+                            <i className="icon-chevron-right"></i>
+                        </div>
+                    </Link>
                 </div>
-
-                <Link to="/playlists" className='options-menu-item-link' onClick={onClick}>
-                    <div className="options-menu-item">
-                        <i className="icon-music"></i>
-                        <span className="options-menu-text">Go to Playlists</span>
-                        <i className="icon-chevron-right"></i>
-                    </div>
-                </Link>
-
-                <Link to="/playlists/create" className='options-menu-item-link' onClick={onClick}>
-                    <div className="options-menu-item">
-                        <i className="icon-plus"></i>
-                        <span className="options-menu-text">Create a Playlist</span>
-                        <i className="icon-chevron-right"></i>
-                    </div>
-                </Link>
-            </div>
-        </div >
+            </div >
+        </>
     );
 }
 
